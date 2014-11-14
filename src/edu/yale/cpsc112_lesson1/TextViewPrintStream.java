591cde95462be1e16f43af088de4f9e3e1b36769
Francesca Casalino <francesca.casalino@yale.edu>
2014-11-13 21:01:59 -0500

    first

---
+++ src/edu/yale/cpsc112_lesson1/TextViewPrintStream.java
@@ -0,0 +1,24 @@
+package edu.yale.cpsc112_lesson1;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import android.app.Activity;
+import android.widget.TextView;
+
+public class TextViewPrintStream extends PrintStream {
+
+	static private ByteArrayOutputStream out = new ByteArrayOutputStream();
+	private TextView textView;
+
+	public TextViewPrintStream(Activity context) {
+		super(out);
+		this.textView = new TextView(context);
+		context.setContentView(textView);
+	}
+
+	public synchronized void print(String str) {
+		super.print(str);
+		textView.setText(out.toString());
+	}
+}

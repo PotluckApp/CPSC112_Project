9bde326523aaaf3dbf12d0f9697316ec4b9c6cc2
francescacaz <francesca.casalino@yale.edu>
2014-11-19 20:14:31 -0500

    Adding file link

--- src/edu/yale/cpsc112_lesson1/MainActivity.java
+++ src/edu/yale/cpsc112_lesson1/MainActivity.java
@@ -10,8 +10,47 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.view.KeyEvent;
 import android.widget.TextView.OnEditorActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
+	private String path;
+	
+	public void ReadFile(String file_path){
+		path = file_path;
+	}
+		
+	public String[] OpenFile() throws IOException{
+		FileReader fr = new FileReader(path);
+		BufferedReader textReader = new BufferedReader(fr);
+		
+		int numberOfLines = readLines();
+		String[ ] textData = new String[numberOfLines];
+		
+		for (int i=0; i < numberOfLines; i++) {
+			textData[ i ] = textReader.readLine();
+			}
+		
+		textReader.close( );
+		return textData;
+		
+	}
+
+	int readLines() throws IOException{
+		FileReader file_to_read = new FileReader(path);
+		BufferedReader bf = new BufferedReader(file_to_read);
+		
+		String aLine;
+		int numberOfLines =0;
+		
+		while ((aLine = bf.readLine()) != null) {
+			numberOfLines++;
+		}
+		bf.close();
+		return numberOfLines;
+	}
+	
 	
 	public static String ingredient = "";
 	public static String ownedIngredients="";
